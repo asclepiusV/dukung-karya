0b2168ea1f70f977944322a6119e7e480ab00de4
Obyyyy <qolbymuhammad86@gmail.com>
2023-06-18 20:15:12 +0700

    s

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,10 +8,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/">Explore</a>
+                        <a class="nav-link" href="/lists">Explore</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Start a Project</a>
