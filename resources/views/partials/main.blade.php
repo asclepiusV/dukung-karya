83d3c604662485709b8102f054fb20df28d65493
Muhammad Khaliq Teuku Ansari <2110817310008@mhs.ulm.ac.id>
2023-06-23 16:15:30 +0800

    tambah icon

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dukung Karya | {{ $title }}</title>
+    <link href="{{asset('storage/images/Logo.png')}}" rel="shortcut icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
