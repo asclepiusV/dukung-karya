35e8087895929a66e002795fbc58ed9202563cb6
Muhammad Khaliq Teuku Ansari <2110817310008@mhs.ulm.ac.id>
2023-06-23 11:38:12 +0800

    admin

---
+++ resources/views/profile/dashboard.blade.php
@@ -0,0 +1,59 @@
+<!-- @extends('partials.main')
+
+@section('container')
+
+<div class="jumbotron text-center content">
+    <h1 class="display-4 mb-4"><b>Selamat datang di Dukung Karya</b></h1>
+    <p class="lead mb-4">Jelajahi proyek luar biasa dan bantu wujudkan menjadi nyata.</p>
+    <a class="btn btn-success btn-lg" href="/start" role="button">Inisiasi Proyek Anda</a>
+</div>
+
+{{-- <div class="container"> --}}
+<div class="popular-projects mb-5 mt-5">
+    <h2 class="text-center mb-5">Featuring Projects</h2>
+    <div class="slider">
+        @foreach ($project as $item)
+        <div class="col-md-4">
+            {{-- <a href="/projects/{{ $item["slug"] }}" class="card-link"> --}}
+                <div class="card project-card">
+                    <img src="{{ asset('storage/images/'.$item['img_project']) }}" class="card-img-top image" alt="Project Image">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $item["project_name"] }}</h5>
+                        <p class="card-text">{{ $item["description"] }}</p>
+                        <a href="/projects/{{ $item["slug"] }}" class="btn btn-success">Support Project</a>
+                    </div>
+                </div>
+                
+            {{-- </a> --}}
+        </div>
+        @endforeach
+    </div>
+    {{-- <div class="slick-next"><i class="fas fa-chevron-right"></i></div> --}}
+</div>
+{{-- </div> --}}
+            {{-- <a href="/projects/{{ $item["slug"] }}" class="card-link"> --}}
+
+{{-- <div class="container">
+    @foreach (array_chunk($project, 3) as $project)
+    <div class="row mb-5">
+        @foreach ($project as $item)
+        <div class="col-md-4">
+                <div class="card project-card">
+                    <img src="{{ asset('storage/images/'.$item['img_project']) }}" class="card-img-top image" alt="Project Image">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $item["project_name"] }}</h5>
+                        <p class="card-text">{{ $item["description"] }}</p>
+                        <a href="/projects/{{ $item["slug"] }}" class="btn btn-primary">Support Project</a>
+                    </div>
+                </div>
+            </a>
+        </div>
+        @endforeach
+    </div>
+    @endforeach
+</div> --}}
+
+@endsection -->
+
+
+<h1>Selamat datang admin</h1>
