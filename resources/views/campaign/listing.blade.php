59527b893e5d07028e50c83c585e97014cf0eb9b
Muhammad Khaliq Teuku Ansari <2110817310008@mhs.ulm.ac.id>
2023-06-23 11:41:23 +0800

    update list

--- resources/views/campaign/listing.blade.php
+++ resources/views/campaign/listing.blade.php
@@ -2,17 +2,17 @@
 
 @section('container')
     <div class="container">
-        <h1 class="mt-5">Kickstarter Home</h1>
+        <h1 class="mt-5 text-center">List Proyek</h1>
 
     @if(count($project))
-        <div class="card mb-3">
+        <!-- <div class="card mb-3">
             <img class="card-img-top" src="" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
-        </div>
+        </div> -->
 
         @foreach (array_chunk($project, 3) as $project)
             <div class="row mb-5">
